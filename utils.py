463e15ae3684f7507995a6a2822359c91ff36453
Sean Kelley <seangtkelley@gmail.com>
2020-06-25 15:46:08 -0400

    hrrrX retrieval func now fills in missing data; ignored hrrrX data

--- utils.py
+++ utils.py
@@ -95,18 +95,35 @@ def get_hrrrx_lake_output(start_date, end_date, cycle_hours=list(range(24)), pre
 
         for cycle in cycle_hours:
             for pred in pred_hours:
-                # build file meta
+
+                # fill meta
+                data = {
+                    'cycle_hour': [cycle]*len(lake_meta['points']),
+                    'fcst_datetime': datetime.fromordinal(curr_date.toordinal()) + timedelta(hours=cycle),
+                    'pred_hour': [pred]*len(lake_meta['points']),
+                    'pred_datetime': datetime.fromordinal(curr_date.toordinal()) + timedelta(hours=pred),
+                    'idx': [ str(point) for point in lake_meta['points'] ],
+                    'depth': lake_meta['depths'],
+                }
+
+                # build file info
                 filename = f'hrrrX.t{cycle:02}z.wrfsfcf{pred:02}'
                 nc4_filepath = os.path.join(DATA_DIR, date_dir, filename+'.nc4' )
 
+                # if not exist, attempt to retrieve grib2 and convert
                 if not os.path.isfile(nc4_filepath):
                     # download grib2
-                    url = os.path.join(grib2_host, date_dir, filename+'.grib2')
                     grb2_filepath = os.path.join(DATA_DIR, date_dir, filename+'.grib2')
-                    download_url_prog(url, grb2_filepath)
+                    if not os.path.isfile(grb2_filepath) or 'xml' in magic.from_file(grb2_filepath, mime=True):
+                        url = os.path.join(grib2_host, date_dir, filename+'.grib2')
+                        download_url_prog(url, grb2_filepath)
 
-                    if 'xml' in magic.from_file(grb2_filepath, mime=True):
-                        break
+                        if 'xml' in magic.from_file(grb2_filepath, mime=True):
+                            print(f"{filename} not avaiable from {grib2_host}")
+
+                            # fill nans
+                            data['water_temp'] = [np.nan]*len(lake_meta['points'])
+                            data['air_temp'] = [np.nan]*len(lake_meta['points'])
 
                     # convert to netCDF4
                     convert_cmd = ["wgrib2", grb2_filepath, "-nc4", "-netcdf", nc4_filepath ]
@@ -115,43 +132,51 @@ def get_hrrrx_lake_output(start_date, end_date, cycle_hours=list(range(24)), pre
 
                     if rc != 0:
                         print(f"Failed to convert {filename} to netcdf")
-                        break
-                
-                # load dataset
-                hrrr_output = Dataset(nc4_filepath, "r", format="NETCDF4")
-                
-                # get temps
-                water_temps = np.squeeze(hrrr_output.variables['TMP_surface'][:].data)
-                air_temps = np.squeeze(hrrr_output.variables['TMP_2maboveground'][:].data)
+                        
+                        # fill nans
+                        data['water_temp'] = [np.nan]*len(lake_meta['points'])
+                        data['air_temp'] = [np.nan]*len(lake_meta['points'])
 
-                lake_water_temps = np.array([ water_temps[tuple(point)] for point in lake_meta['points'] ])
-                lake_air_temps = np.array([ air_temps[tuple(point)] for point in lake_meta['points'] ])
+                # ensure nc4 file exists
+                if os.path.isfile(nc4_filepath):
+                    # load dataset
+                    try:
+                        hrrr_output = Dataset(nc4_filepath, "r", format="NETCDF4")
+                        
+                        # get temps
+                        water_temps = np.squeeze(hrrr_output.variables['TMP_surface'][:].data)
+                        air_temps = np.squeeze(hrrr_output.variables['TMP_2maboveground'][:].data)
 
-                # convert K to C
-                lake_water_temps = lake_water_temps - 272.15
-                lake_air_temps = lake_air_temps - 272.15
+                        lake_water_temps = np.array([ water_temps[tuple(point)] for point in lake_meta['points'] ])
+                        lake_air_temps = np.array([ air_temps[tuple(point)] for point in lake_meta['points'] ])
 
-                # load lake points as geodataframe
-                df = pd.DataFrame(data={
-                    'cycle_hour': [cycle]*len(lake_water_temps),
-                    'fcst_datetime': datetime.fromordinal(curr_date.toordinal()) + timedelta(hours=cycle),
-                    'pred_hour': [pred]*len(lake_water_temps),
-                    'pred_datetime': datetime.fromordinal(curr_date.toordinal()) + timedelta(hours=pred),
-                    'idx': [ str(point) for point in lake_meta['points'] ],
-                    'depth': lake_meta['depths'],
-                    'water_temp': lake_water_temps, 
-                    'air_temp': lake_air_temps,
-                })
+                        # convert K to C
+                        lake_water_temps = lake_water_temps - 272.15
+                        lake_air_temps = lake_air_temps - 272.15
 
-                gdf = gpd.GeoDataFrame(df, crs='epsg:4326', geometry=[ Point(xy) for xy in zip(lake_meta['longs'], lake_meta['lats']) ])
-                gdf = gdf.to_crs(epsg=3857)
+                        # load lake points as geodataframe
+                        data['water_temp'] = lake_water_temps
+                        data['air_temp'] = lake_air_temps
 
-                if lakes_gdf is None:
-                    lakes_gdf = gdf
+                        hrrr_output.close()
+
+                    except:
+                        print(f"Failed to read data from {nc4_filepath}")
+                        
+                        # fill nans
+                        data['water_temp'] = [np.nan]*len(lake_meta['points'])
+                        data['air_temp'] = [np.nan]*len(lake_meta['points'])
+                    
+                # failsafe (no nc4 and no grib2)
                 else:
-                    lakes_gdf = lakes_gdf.append(gdf, ignore_index=True)
-
-                hrrr_output.close()
+                    # fill nans
+                    data['water_temp'] = [np.nan]*len(lake_meta['points'])
+                    data['air_temp'] = [np.nan]*len(lake_meta['points'])
+                
+                # append to geodataframe
+                df = pd.DataFrame(data=data)
+                gdf = gpd.GeoDataFrame(df, crs='epsg:4326', geometry=[ Point(xy) for xy in zip(lake_meta['longs'], lake_meta['lats']) ])
+                lakes_gdf = gdf if lakes_gdf is None else lakes_gdf.append(gdf, ignore_index=True)
 
         curr_date += timedelta(days=1)
     
